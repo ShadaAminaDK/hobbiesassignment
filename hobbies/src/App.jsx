4c4e6bced4ca8fc91241c0e017a1e8c9f3760073
ShadaAminaDK <shadaaminadk@gmail.com>
2025-08-25 14:56:32 +0530

    updated

---
+++ hobbies/src/App.jsx
@@ -0,0 +1,32 @@
+import Card from './components/Card'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <Card
+     title="Card Title"
+     description="This is a description of the card."
+     imageUrl="https://cff2.earth.com/uploads/2018/07/25115124/Kids-now-spend-twice-as-much-time-playing-indoors-than-outdoors.jpg"
+     />
+
+     <Card
+     title="Card Title"
+     description="This is a description of the card."
+     imageUrl="https://tse4.mm.bing.net/th/id/OIP.vAiM0e-YfxOgun3dnINsrgHaE8?pid=Api&P=0&h=180"
+     />
+
+     <Card
+     title="Card Title"
+     description="This is a description of the card."
+     imageUrl="https://tse3.mm.bing.net/th/id/OIP.JRYX72Z84A2welJlpkeGWQHaE8?pid=Api&P=0&h=180"
+     />
+    
+     
+    </>
+  )
+  
+}
+
+export default App
